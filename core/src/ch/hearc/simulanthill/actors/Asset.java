ce2ebf3d612bb5e8b2cf4eb7e06ee0cbffaf6ff3
Titiou <tania.taniat@hotmail.com>
2021-04-29 16:46:42 +0200

    Add image assets in Asset Class

--- core/src/ch/hearc/simulanthill/actors/Asset.java
+++ core/src/ch/hearc/simulanthill/actors/Asset.java
@@ -7,6 +7,9 @@ import com.badlogic.gdx.graphics.Texture;
 public class Asset {
     static public AssetManager manager = new AssetManager();
     static private AssetDescriptor<Texture> ant = new AssetDescriptor<Texture>("ant.png", Texture.class);
+    static private AssetDescriptor<Texture> anthill = new AssetDescriptor<Texture>("anthill.png", Texture.class);
+    static private AssetDescriptor<Texture> obstacle = new AssetDescriptor<Texture>("obstacle.png", Texture.class);
+    static private AssetDescriptor<Texture> resource = new AssetDescriptor<Texture>("resource.png", Texture.class);
     static public void loadAssets() {
 		manager.load(Asset.ant);
 		//manager.load(new AssetDescriptor<Texture>("badlogic.jpg", Texture.class));
@@ -16,6 +19,21 @@ public class Asset {
     static public Texture ant()
     {
         return manager.get(ant);
+    }
+
+    static public Texture anthill()
+    {
+        return manager.get(anthill);
+    }
+
+    static public Texture obstacle()
+    {
+        return manager.get(obstacle);
+    }
+
+    static public Texture resource()
+    {
+        return manager.get(resource);
     }
 
 }
